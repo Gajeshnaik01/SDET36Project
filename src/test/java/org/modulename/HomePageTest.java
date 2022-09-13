5535d320a831a455e2e94979ad7d1d762b6627a7
Gajeshnaik01 <gajesh.naik1413@gamil.com>
2022-09-13 15:55:05 +0530

    message

---
+++ src/test/java/org/modulename/HomePageTest.java
@@ -0,0 +1,12 @@
+package org.modulename;
+
+public class HomePageTest {
+
+	public static void main(String[] args) {
+		System.out.println("username");
+		System.out.println("password");
+		System.out.println("Login");
+
+	}
+
+}
